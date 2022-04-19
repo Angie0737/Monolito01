f00248acba92d464b778b2736975269a2269a8c1
Angie0737 <judithecheverry24@gmail.com>
2022-04-19 16:18:27 -0500

    Plantilla Del Repositorio

--- WebApplicationEmployess/WebApplicationEmployess/Models/Employee.cs
+++ WebApplicationEmployess/WebApplicationEmployess/Models/Employee.cs
@@ -1,6 +1,14 @@
 ﻿namespace WebApplicationEmployess.Models
 {
-    public class Employes
+    public class Employee
     {
+
+        public int Id { get; set; }
+       
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public string Adress { get; set; }
+        public DateTime CreatedDate { get; set; }
+
     }
 }
